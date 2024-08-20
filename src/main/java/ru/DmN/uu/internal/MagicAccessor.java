e6e664b389d261e4a3170d23d993ccbb8416c875
DomamaN202 <vip.domaman@mail.ru>
2024-08-20 19:42:04 +0700

    hm...

--- src/main/java/ru/DmN/uu/internal/MagicAccessor.java
+++ src/main/java/ru/DmN/uu/internal/MagicAccessor.java
@@ -27,51 +27,99 @@ public class MagicAccessor {
     public static final sun.misc.Unsafe unsafe = (Unsafe) new FieldBuilder("sun/misc/Unsafe", "theUnsafe", "Lsun/misc/Unsafe;").getA();
 
     public static void init() {
-        new CallBuilder("addExportsToAllUnnamed0", "(Ljava/lang/Module;Ljava/lang/String;)V", "java/lang/Module").arg(BytecodeUtils.ldc$class("jdk/internal/reflect/DmNMagicAccessor").getModule()).arg("jdk.internal.reflect").invokeStatic(false).end();
+        new CallBuilder("addExportsToAllUnnamed0", "(Ljava/lang/Module;Ljava/lang/String;)V", "java/lang/Module")
+                .arg(BytecodeUtils.ldc$class("jdk/internal/reflect/DmNMagicAccessor").getModule())
+                .arg("jdk.internal.reflect")
+                .invokeStatic(false)
+                .end();
     }
 
     public static MethodHandle make(Object memberName) {
-        return (MethodHandle) new CallBuilder("make", "(Ljava/lang/invoke/MemberName;)Ljava/lang/invoke/DirectMethodHandle;", "java/lang/invoke/DirectMethodHandle").arg(lookup).arg(memberName).invokeStatic(false).endA();
+        return (MethodHandle)
+                new CallBuilder("make", "(Ljava/lang/invoke/MemberName;)Ljava/lang/invoke/DirectMethodHandle;", "java/lang/invoke/DirectMethodHandle")
+                        .arg(lookup)
+                        .arg(memberName)
+                        .invokeStatic(false)
+                        .endA();
     }
 
     public static boolean isResolved(Object memberName) {
-        return new CallBuilder("isResolved", "()Z", "java/lang/invoke/MemberName").arg(memberName).invokeVirtual().endZ();
+        return new CallBuilder("isResolved", "()Z", "java/lang/invoke/MemberName")
+                .arg(memberName)
+                .invokeVirtual()
+                .endZ();
     }
 
     public static Object createMemberName(Class<?> refc, String name, MethodType type, byte refKind) {
-        return new CallBuilder("<init>", "(Ljava/lang/Class;Ljava/lang/String;Ljava/lang/invoke/MethodType;B)V", "java/lang/invoke/MemberName").alloc().arg(refc).arg(name).arg(type).arg(refKind).invokeSpecial(false).endA();
+        return new CallBuilder("<init>", "(Ljava/lang/Class;Ljava/lang/String;Ljava/lang/invoke/MethodType;B)V", "java/lang/invoke/MemberName")
+                .alloc()
+                .arg(refc)
+                .arg(name)
+                .arg(type)
+                .arg(refKind)
+                .invokeSpecial(false)
+                .endA();
     }
 
     public static Object resolveOrFail(byte refKind, Class<?> refc, String name, Class<?> type) {
-        return new CallBuilder("resolveOrFail", "(BLjava/lang/Class;Ljava/lang/String;Ljava/lang/Class;)Ljava/lang/invoke/MemberName;", "java/lang/invoke/MethodHandles$Lookup").arg(lookup).arg(refKind).arg(refc).arg(name).arg(type).invokeVirtual().endA();
+        return new CallBuilder("resolveOrFail", "(BLjava/lang/Class;Ljava/lang/String;Ljava/lang/Class;)Ljava/lang/invoke/MemberName;", "java/lang/invoke/MethodHandles$Lookup")
+                .arg(lookup)
+                .arg(refKind)
+                .arg(refc)
+                .arg(name)
+                .arg(type)
+                .invokeVirtual()
+                .endA();
     }
-
+//
     public static Object resolveOrFail(byte refKind, Object member, Class<?> clazz) {
-        return new CallBuilder("resolveOrFail", "(BLjava/lang/invoke/MemberName;Ljava/lang/Class;ILjava/lang/Class;)Ljava/lang/invoke/MemberName;", "java/lang/invoke/MemberName$Factory").arg(IMPL_NAMES).arg(refKind).arg(BytecodeUtils.checkcast(member, "java/lang/invoke/MemberName")).arg(clazz).arg(MethodHandles$ALL).arg(NoSuchMethodException.class).invokeVirtual().endA();
+        return new CallBuilder("resolveOrFail", "(BLjava/lang/invoke/MemberName;Ljava/lang/Class;ILjava/lang/Class;)Ljava/lang/invoke/MemberName;", "java/lang/invoke/MemberName$Factory")
+                .arg(IMPL_NAMES)
+                .arg(refKind)
+                .arg(BytecodeUtils.checkcast(member, "java/lang/invoke/MemberName"))
+                .arg(clazz)
+                .arg(MethodHandles$ALL)
+                .arg(NoSuchMethodException.class)
+                .invokeVirtual()
+                .endA();
     }
 
     public static Object resolve(byte refKind, Object ref, Class<?> lookupClass, int allowedModes, boolean speculativeResolve) {
-        return new CallBuilder("resolve", "(BLjava/lang/invoke/MemberName;Ljava/lang/Class;IZ)Ljava/lang/invoke/MemberName;", "java/lang/invoke/MemberName$Factory").arg(IMPL_NAMES).arg(refKind).arg(BytecodeUtils.checkcast(ref, "java/lang/invoke/MemberName")).arg(lookupClass).arg(allowedModes).arg(speculativeResolve).invokeVirtual().endA();
+        return new CallBuilder("resolve", "(BLjava/lang/invoke/MemberName;Ljava/lang/Class;IZ)Ljava/lang/invoke/MemberName;", "java/lang/invoke/MemberName$Factory")
+                .arg(IMPL_NAMES)
+                .arg(refKind)
+                .arg(BytecodeUtils.checkcast(ref, "java/lang/invoke/MemberName"))
+                .arg(lookupClass)
+                .arg(allowedModes)
+                .arg(speculativeResolve)
+                .invokeVirtual()
+                .endA();
     }
 
     public static VarHandle makeFieldHandle(Field field, Class<?> refc, Class<?> type) {
-        var member = new CallBuilder("<init>", "(Ljava/lang/reflect/Field;)V", "java/lang/invoke/MemberName").alloc().arg(field).invokeSpecial(false).endA();
-        return (VarHandle) new CallBuilder("makeFieldHandle", "(Ljava/lang/invoke/MemberName;Ljava/lang/Class;Ljava/lang/Class;Z)Ljava/lang/invoke/VarHandle;", "java/lang/invoke/VarHandles").arg(member).arg(refc).arg(type).arg(true).invokeStatic(false).endA();
+        var member = new CallBuilder("<init>", "(Ljava/lang/reflect/Field;)V", "java/lang/invoke/MemberName")
+                .alloc()
+                .arg(field)
+                .invokeSpecial(false)
+                .endA();
+        return (VarHandle)
+                new CallBuilder("makeFieldHandle", "(Ljava/lang/invoke/MemberName;Ljava/lang/Class;Ljava/lang/Class;Z)Ljava/lang/invoke/VarHandle;", "java/lang/invoke/VarHandles")
+                        .arg(member)
+                        .arg(refc)
+                        .arg(type)
+                        .arg(true)
+                        .invokeStatic(false)
+                        .endA();
     }
 
     public static VarHandle unreflect(Field field) {
         return makeFieldHandle(field, field.getDeclaringClass(), field.getType());
     }
-
     public static void addExportsToAllUnnamed0(Module module, String pkg) {
-        new CallBuilder("addExportsToAllUnnamed0", "(Ljava/lang/Module;Ljava/lang/String;)V", "java/lang/Module").arg(module).arg(pkg).invokeStatic(false).end();
-    }
-
-    public static long map0(FileChannel channel, int prot, long position, long length, boolean isSync) {
-        return new CallBuilder("map0", "(IJJZ)J", "sun/nio/ch/FileChannelImpl").arg(channel).arg(prot).arg(position).arg(length).arg(isSync).invokeVirtual().endL();
-    }
-
-    public static int unmap0(long address, long length) {
-        return new CallBuilder("unmap0", "(LL)I", "sun/nio/ch/FileChannelImpl").arg(address).arg(length).invokeStatic(false).endI();
+        new CallBuilder("addExportsToAllUnnamed0", "(Ljava/lang/Module;Ljava/lang/String;)V", "java/lang/Module")
+                .arg(module)
+                .arg(pkg)
+                .invokeStatic(false)
+                .end();
     }
 }
